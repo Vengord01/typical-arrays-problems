4d8ed3cee4db05acd44d449112e908337e3d64a0
Vengord01 <64409652+Vengord01@users.noreply.github.com>
2021-08-18 13:21:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  
+  let avg = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    avg += array[i];
+  }
+
+  avg /= array.length;
+  return avg;
 }
